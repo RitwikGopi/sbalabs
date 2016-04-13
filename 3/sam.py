4962fab9f2eb873b13c265fd8720fea5824f9449
RitwikGopi <ritwikgopi@gmail.com>
2016-04-13 21:00:08 +0530

    correction

--- 3/sam.py
+++ 3/sam.py
@@ -16,5 +16,5 @@ while True:
     select_r, select_w, select_x = select([sock], [], [])
     for s in select_r:
 	dat, addr = s.recvfrom(1024)
-	print ord(dat)
-	#call(c + [str(2.5 * ord(dat[0])),str( 1.5 * ord(dat[1]))])
+	#print ord(dat)
+	call(c + [str(2.5 * ord(dat[0])),str( 1.5 * ord(dat[1]))])
